48a3eb42786a7d8e96237eb4d366dd7e4e26e804
Jamie Holloway <jamie@cadiahq.com>
2024-01-30 14:26:03 +0000

    added vercel as host

--- pissing_the_map/settings.py
+++ pissing_the_map/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-l+z96^j!zl)e4b*ar2f3ga24=63#j0m-^8%$+)iy$b34g11f#z"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pissing-the-map.vercel.app']
 
 
 # Application definition
