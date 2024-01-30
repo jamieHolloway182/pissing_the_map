1945197f6383f9ad70c933857f91a1c8486acc43
Jamie Holloway <jamie@cadiahq.com>
2024-01-30 15:14:10 +0000

    cosmetic changes and changed url

--- pissing_the_map/urls.py
+++ pissing_the_map/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("pissmap/", include("pissmap.urls")),
+    path("", include("pissmap.urls")),
     path("admin/", admin.site.urls),
 ]
 
